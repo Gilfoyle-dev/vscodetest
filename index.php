f42d395f1cb16ab7eed85127e8946a7a275d34a9
Gilfoyle-666 <bertramgilfoyle@tutanota.com>
2024-07-02 01:41:26 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require 'vendor/autoload.php';
+$client = new \GuzzleHttp\Client();
+$response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+
+echo $response->getStatusCode(); 
